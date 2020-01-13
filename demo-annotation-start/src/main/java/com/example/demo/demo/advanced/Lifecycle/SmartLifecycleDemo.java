41099508d2670172c8b7d30dc56b74054816548b
tsc <tshch1989@sina.com>
2020-01-13 15:01:10 +0800

    基础演示

---
+++ demo-annotation-start/src/main/java/com/example/demo/demo/advanced/Lifecycle/SmartLifecycleDemo.java
@@ -0,0 +1,19 @@
+package com.example.demo.demo.advanced.Lifecycle;
+
+import com.example.demo.demo.advanced.Lifecycle.auxiliary.LifecycleHello;
+import com.example.demo.demo.advanced.Lifecycle.auxiliary.MySmartLifecycle;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SmartLifecycleDemo {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.registerBean(LifecycleHello.class);
+        context.registerBean(MySmartLifecycle.class);
+        context.refresh();
+
+        LifecycleHello lifecycleHello = context.getBean(LifecycleHello.class);
+        lifecycleHello.say();
+    }
+
+}
