658f35a2c47c4064953c6979c5973bbe5a22a767
tsc <tshch1989@sina.com>
2020-01-13 15:08:37 +0800

    基础演示

---
+++ demo-start/src/main/java/com/example/demo/demo/advanced/scope/auxiliary/ScopeHello.java
@@ -0,0 +1,12 @@
+package com.example.demo.demo.advanced.scope.auxiliary;
+
+import org.springframework.context.annotation.Scope;
+
+@Scope("myScope")
+public class ScopeHello {
+
+    public void say() {
+        System.out.println("Hello myScope...");
+    }
+
+}
