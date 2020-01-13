396aa909c6b16a31e0485b9b0771c21d76026cec
tsc <tshch1989@sina.com>
2020-01-13 11:22:29 +0800

    基础演示

---
+++ demo-annotation-start/src/main/java/com/example/demo/demo/advanced/scope/ScopeDemo.java
@@ -0,0 +1,44 @@
+package com.example.demo.demo.advanced.scope;
+
+import com.example.demo.demo.advanced.scope.auxiliary.MyScope;
+import com.example.demo.demo.advanced.scope.auxiliary.ScopeHello;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
+import org.springframework.beans.factory.config.Scope;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.function.Supplier;
+
+public class ScopeDemo {
+
+    public static void main(String[] args) {
+
+        CustomScopeConfigurer customScopeConfigurer = new CustomScopeConfigurer();
+        customScopeConfigurer.addScope("myScope", new MyScope());
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.registerBean(CustomScopeConfigurer.class, new Supplier<CustomScopeConfigurer>() {
+            @Override
+            public CustomScopeConfigurer get() {
+                return customScopeConfigurer;
+            }
+        });
+        context.registerBean(ScopeHello.class);
+        context.refresh();
+
+        //直接读取，对象一致
+        ScopeHello scopeHello = context.getBean(ScopeHello.class);
+        System.out.println(scopeHello);
+        ScopeHello scopeHello1 = context.getBean(ScopeHello.class);
+        System.out.println(scopeHello1);
+
+        //删除创建的对象
+        Scope myScope = context.getBeanFactory()
+                .getRegisteredScope("myScope");
+        myScope.remove("scopeHello");
+
+        //产生薪的对象
+        ScopeHello scopeHello2 = context.getBean(ScopeHello.class);
+        System.out.println(scopeHello2);
+    }
+
+}
